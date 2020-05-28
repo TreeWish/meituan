e8572ea50a63db36b704b5a9310d1e11db2a47e1
魏云龙 <1131093754@qq.com>
2020-05-28 22:01:17 +0800

    detail finish

---
+++ components/detail/crumbs.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="m-crumbs">
+    <el-breadcrumb separator=">">
+      <el-breadcrumb-item></el-breadcrumb-item> <el-breadcrumb-item :to="{ path: '/' }">{{ $store.state.geo.position.city.replace('市','') }}美团</el-breadcrumb-item>
+      <el-breadcrumb-item :to="{ path: '/' }">{{ $store.state.geo.position.city.replace('市','') }}{{ type }}</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="#">{{ $store.state.geo.position.city.replace('市','') }}{{ decodeURIComponent(keyword) }}</a></el-breadcrumb-item>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    keyword: String,
+    type: String
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- server/api/geo.js
+++ server/api/geo.js
@@ -27,6 +27,10 @@ router.get('/getPosition', async (ctx) => {
 });
 
 router.get('/menu', async (ctx) => {
+ // const result = await Menu.findOne()
+  // ctx.body = {
+  //   menu: result.menu
+  // }
   let {status, data: {menu}} = await axios.get('http://cp-tools.cn/geo/menu');
   if (status === 200) {
     ctx.body = {
@@ -47,6 +51,14 @@ router.get('/province', async (ctx) => {
 });
 
 router.get('/province/:id', async (ctx) => {
+   // let city = await City.findOne({id: ctx.params.id})
+  //
+  // ctx.body = {
+  //   code: 0,
+  //   city: city.value.map(item => {
+  //     return {province: item.province, id: item.id, name: item.name}
+  //   })
+  // }
   let {status, data: {city}} = await axios.get(`http://cp-tools.cn/geo/province/${ctx.params.id}`);
   ctx.body = {
     city: status===200 ? city : []
@@ -54,6 +66,23 @@ router.get('/province/:id', async (ctx) => {
 });
 
 router.get('/city', async (ctx) => {
+   // let city = []
+  // let result = await City.find()
+  // result.forEach(item => {
+  //   city = city.concat(item.value)
+  // })
+  // ctx.body = {
+  //   code: 0,
+  //   city: city.map(item => {
+  //     return {
+  //       province: item.province,
+  //       id: item.id,
+  //       name: item.name === '市辖区' || item.name === '省直辖县级行政区划'
+  //         ? item.province
+  //         : item.name
+  //     }
+  //   })
+  // }
   let {status, data: {city}} = await axios.get('http://cp-tools.cn/geo/city');
   ctx.body = {
     city: status===200 ? city : []
@@ -61,6 +90,26 @@ router.get('/city', async (ctx) => {
 });
 
 router.get('/hotCity', async (ctx) => {
+  // let list = [
+  //   '北京市',
+  //   '上海市',
+  //   '广州市',
+  //   '深圳市',
+  //   '天津市',
+  //   '西安市',
+  //   '杭州市',
+  //   '南京市',
+  //   '武汉市',
+  //   '成都市'
+  // ]
+  // let result = await City.find()
+  // let nList = []
+  // result.forEach(item => {
+  //   nList = nList.concat(item.value.filter(k => list.includes(k.name) || list.includes(k.province)))
+  // })
+  // ctx.body = {
+  //   hots: nList
+  // }
   let {status, data: {hots}} = await axios.get('http://cp-tools.cn/geo/hotCity');
   ctx.body = {
     hots: status===200 ? hots : []
